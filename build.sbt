1ed4b0386e07f946e3da954a3d6eaa2be39b36fc
Dovid Kopel <dovid@dovidkopel.com>
2017-01-18 09:43:42 -0500

    Added base project

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "intro-to-scala-for-java-devs"
+
+version := "1.0"
+
+scalaVersion := "2.12.1"
---
+++ sample-code/src/main/scala/com/dovidkopel/scala/HelloWorld.scala
@@ -0,0 +1,8 @@
+package com.dovidkopel.scala
+
+/**
+  * Created by dkopel on 1/18/17.
+  */
+object HelloWorld extends App {
+    println("Hello world!")
+}
